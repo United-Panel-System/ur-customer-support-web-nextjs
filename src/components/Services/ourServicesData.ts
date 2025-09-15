9d1fc254f4470cc67c9e45907f9ad206df04a7a3
Jingyi <jingyi012@gmail.com>
2025-09-16 00:23:18 +0800

    Update service section

--- src/components/Services/ourServicesData.ts
+++ src/components/Services/ourServicesData.ts
@@ -5,6 +5,7 @@ import {
   Wrench,
   Handshake,
   HeadphonesIcon,
+  Hammer,
 } from "lucide-react";
 
 const ourServicesData = [
@@ -16,9 +17,15 @@ const ourServicesData = [
   },
   {
     icon: Ruler,
-    title: "Design & Installation",
+    title: "Design",
     description:
-      "Our engineers will design the perfect layout and our trained workers will install the cold room according to the highest standards.",
+      "Our team of trained engineers and draftsmen will compute all the technical data, design and provide layout plans/drawings on the proposed cold room.",
+  },
+  {
+    icon: Hammer,
+    title: "Installation",
+    description:
+      "Our trained workers will install the cold room according to the highest standards.",
   },
   {
     icon: CheckSquare,
@@ -27,22 +34,16 @@ const ourServicesData = [
       "Our refrigeration systems are thoroughly tested and tuned to ensure optimal performance before project handover.",
   },
   {
-    icon: Wrench,
-    title: "Service & Maintenance",
-    description:
-      "We provide periodic service and maintenance to ensure your cold room continues to perform at its best.",
-  },
-  {
     icon: Handshake,
     title: "Handover",
     description:
       "Upon completion, the project is formally handed over to the project owner with all documentation and final adjustments.",
   },
   {
-    icon: HeadphonesIcon,
-    title: "Customer Support",
+    icon: Wrench,
+    title: "Service & Maintenance",
     description:
-      "Our customer support team is always ready to assist you with any questions or concerns after the project is complete.",
+      "We provide periodic service and maintenance to ensure your cold room continues to perform at its best.",
   },
 ];
 
